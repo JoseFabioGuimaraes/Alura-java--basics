b438d1e3125cccaaf39bddd793ee6f3dd29a5565
JoseFabioGuimaraes <jfabioguimaraes@gmail.com>
2025-02-25 11:09:07 -0300

    Uso do leitor Scanner

--- primeiroProjetoAlura/src/Leitura.java
+++ primeiroProjetoAlura/src/Leitura.java
@@ -1,5 +1,17 @@
+
+import java.util.Scanner;
+
 public class Leitura {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+        Scanner leitor = new Scanner(System.in);
+
+        System.out.println("Digite o seu filme favorito");
+        String filmeDigitado = leitor.nextLine();
+        System.out.println("Qual foi a data de lançamento do filme "+filmeDigitado);
+        int anoLancamento = leitor.nextInt();
+        System.out.println("Ano de lançamento é: " +  "\nAgora digite sua avaliação para o filme:");
+        double avaliacaoUsuario = leitor.nextDouble();
+        System.out.println("A sua avaliação foi de: " + avaliacaoUsuario);
 
     }
 }
